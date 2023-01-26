29bb5cfafd5721a44ba5babce4749820d2a6c138
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 10:49:54 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name onb-us-mkt-exp-env --image nexus.sigame.com.br/fission-onboarding-us-market-expirience-ben:0.2.0-1 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name onb-us-mkt-exp-fn --env onb-us-mkt-exp-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission env create --spec --name onb-us-mkt-exp-env --image nexus.sigame.com.br/fission-onboarding-us-market-expirience-ben:0.2.0-1 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name onb-us-mkt-exp-fn --env onb-us-mkt-exp-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
 fission route create --spec --name onb-us-mkt-exp-rt --method PUT --url /onboarding/update_experience_time --function onb-us-mkt-exp-fn
